9e8b78001093af031ace1d0587760468839bc2c9
aabell8 <austinabell8@gmail.com>
2018-03-24 21:17:27 -0400

    Fixed Fifo contract

--- contracts/Fifo.sol
+++ contracts/Fifo.sol
@@ -1,29 +1,27 @@
 pragma solidity ^0.4.19;
-contract Payment {
-    bool public valid;
-    address public buyerAddress;
-    function Payment() public {
-        valid = true;
-        buyerAddress = msg.sender;
-    }
-    
-    function setValid(bool _valid) public {
-        valid = _valid;
-    }
-  
-}
+
 contract FIFO {
-    Payment[] public fifoQueue;
+
+    address[] public fifoQueue;
     uint public cursorPosition;
-    function push() 
+
+    function queueDepth()
+        public
+        constant
+        returns(uint _queueDepth)
+    {
+        return fifoQueue.length - cursorPosition;
+    }
+
+    function push()
         public
         returns(uint jobNumber)
     {
         if(fifoQueue.length + 1 < fifoQueue.length) revert(); // exceeded 2^256 push requests
-        Payment newPayment = new Payment();
-        return fifoQueue.push(newPayment) - 1;
+        return fifoQueue.push(msg.sender) - 1;
     }
-    function pop() 
+
+    function pop()
         public
         returns(uint, address)
     {
@@ -33,24 +31,31 @@ contract FIFO {
         return (cursorPosition -1, fifoQueue[cursorPosition -1]);
     }
 }
+
 contract FifoClient {
+
     FIFO public jobQueue;
     address public ownerAddress;
+
     event LogPush(address sender, uint jobNumber);
     event LogPop (address sender, uint jobNumber);
+
     function FifoClient() public payable {
         jobQueue = new FIFO();
         ownerAddress = msg.sender;
-    } 
+    }
+
     function push()
         public
+        payable
         returns(uint jobNumber)
     {
         uint jobNum = jobQueue.push();
         LogPush(msg.sender, jobNum);
         return jobNum;
     }
-    function pop() 
+
+    function pop()
         public
         returns(uint, address)
     {
@@ -61,21 +66,28 @@ contract FifoClient {
         payoutToSeller();
         return(jobNum, jobVal);
     }
+
     function getQueue() external view returns (FIFO) {
         return jobQueue;
     }
-    
-    function() public payable { 
+
+    function queueLength() external view returns (uint) {
+        jobQueue.queueDepth();
     }
-    
+
+    function() public payable {
+    }
+
     function payoutToSeller() private {
-        ownerAddress.transfer(1);  
+        ownerAddress.transfer(1);
     }
+
     function refundToBuyer(address _buyerAddress) private {
         _buyerAddress.transfer(1);
     }
-    
+
     function getBalance(address _address) external view returns (uint){
         return _address.balance;
     }
+
 }
