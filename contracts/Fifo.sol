359412354b22ba09037f8e82900e74e97482a512
aabell8 <austinabell8@gmail.com>
2018-03-25 09:45:46 -0400

    cleaned up files

--- contracts/Fifo.sol
+++ contracts/Fifo.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.19;
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
 contract FIFO {
+
     address[] public fifoQueue;
     uint public cursorPosition;
+
     function queueDepth()
         public
         constant
@@ -10,6 +12,7 @@ contract FIFO {
     {
         return fifoQueue.length - cursorPosition;
     }
+
     function push()
         public
         returns(uint jobNumber)
@@ -17,6 +20,7 @@ contract FIFO {
         if(fifoQueue.length + 1 < fifoQueue.length) revert(); // exceeded 2^256 push requests
         return fifoQueue.push(msg.sender) - 1;
     }
+
     function pop()
         public
         returns(uint, address)
@@ -27,18 +31,22 @@ contract FIFO {
         return (cursorPosition -1, fifoQueue[cursorPosition -1]);
     }
 }
+
 contract FifoClient {
-    using SafeMath for uint256;
+
     FIFO public jobQueue;
     address public ownerAddress;
     uint256 private standardAmount; //This is just my hack solution to storing value in each address
+
     event LogPush(address sender, uint jobNumber);
     event LogPop (address sender, uint jobNumber);
-    function FifoClient() public payable {
+
+    function FifoClient() public {
         jobQueue = new FIFO();
         ownerAddress = msg.sender;
         standardAmount = 0;
     }
+
     function push()
         public
         payable
@@ -49,32 +57,41 @@ contract FifoClient {
         LogPush(msg.sender, jobNum);
         return jobNum;
     }
+
     function pop()
         public
-        returns(address)
+        returns(uint, address)
     {
         uint jobNum;
         address jobVal;
         (jobNum, jobVal) = jobQueue.pop();
         LogPop(msg.sender, jobNum);
         payoutToSeller();
-        return(jobVal);
+        return(jobNum, jobVal);
     }
+
     function getQueue() external view returns (FIFO) {
         return jobQueue;
     }
+
     function queueLength() external view returns (uint) {
-        return jobQueue.queueDepth();
+        uint test = jobQueue.queueDepth();
+        return test;
     }
+
     function() public payable {
     }
+
     function payoutToSeller() private {
         ownerAddress.transfer(standardAmount);
     }
+
     function refundToBuyer(address _buyerAddress) private {
         _buyerAddress.transfer(standardAmount);
     }
+
     function getBalance(address _address) external view returns (uint){
         return _address.balance;
     }
+
 }
--- contracts/Queue.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.19;
-
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-
-contract Queue is Ownable {
-	using SafeMath for uint256;
-}
